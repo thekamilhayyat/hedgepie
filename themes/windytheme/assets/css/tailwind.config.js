5a8df8d83e58d8933b5ccd9bd5af0e323fe46426
kamilRiaz <hayyat552@gmail.com>
2024-06-03 16:07:53 +0500

    complete web app

---
+++ themes/windytheme/assets/css/tailwind.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+    theme: {
+     
+      extend: {
+        fontFamily: {
+          syne: ['Syne', 'sans-serif'],
+        },
+      },
+      fontSize: {
+        sm: '0.8rem',
+        base: '1rem',
+        xl: '1.25rem',
+        lg: '1.25rem',
+        '2xl': '1.563rem',
+        '3xl': '1.953rem',
+        '4xl': '2.441rem',
+        '5xl': '3.052rem',
+        '6xl': '3.5rem',
+      },
+      colors: {
+        'primary': '#8C82FF',
+        'secondary': '#41007E', 
+        'fontColor':'#010234BF',
+        'fontDark ' : '#010234',
+        'themeWhite': '#FBFBFF',
+        'borderColor': '#01023414',
+        // old
+        'navyBlue': '#143D59',
+        'navyBlueText': '#5C6A77',
+        'navyBlueLight': '#EAF3FA',
+        'black': '#2B2B2B',
+        'white': '#ffffff',
+        'footerText': '#646E75',
+        'themeLightNavy': '#CEDFEB',
+        'nav': '#8598AA',
+      },
+    },
+    variants: {},
+    plugins: []
+  }
